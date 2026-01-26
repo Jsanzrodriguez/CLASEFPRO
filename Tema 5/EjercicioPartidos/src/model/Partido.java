2650500ec80ff37eb15ed04c3032ed6780432911
Jsanzrodriguez <jsanzrodriguez9@gmail.com>
2026-01-26 18:37:40 +0100

    update

---
+++ Tema 5/EjercicioPartidos/src/model/Partido.java
@@ -0,0 +1,92 @@
+package model;
+
+public class Partido {
+
+    private Equipo equipoLocal, equipoVisitante;
+
+    private int gLocal, gVisitante;
+    private boolean jugado;
+
+    //Equipo Albacete
+    //Equipo Madrid
+    //Albacete 3 - Madrid 2
+    //Albacete 3 - Barcelona 2
+    public Partido(){}
+
+    public Partido(Equipo equipoLocal, Equipo equipoVisitante) {
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public Partido(int gLocal, int gVisitante, boolean jugado) {
+        this.gLocal = gLocal;
+        this.gVisitante = gVisitante;
+        this.jugado = jugado;
+    }
+
+    public Partido(Equipo equipoLocal, Equipo equipoVisitante, int gLocal, int gVisitante) {
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.gLocal = gLocal;
+        this.gVisitante = gVisitante;
+        equipoLocal.setgFavor(equipoLocal.getgFavor()+gLocal);
+        equipoLocal.setgContra(equipoLocal.getgContra()+gVisitante);
+        equipoVisitante.setgFavor(equipoVisitante.getgFavor()+gVisitante);
+        equipoVisitante.setgContra(equipoVisitante.getgContra()+gLocal);
+        jugado = true;
+    }
+
+    public void jugarPartido(){
+        if (!jugado){
+            gLocal = (int) (Math.random()*4);
+            gVisitante = (int) (Math.random()*4);
+            jugado = true;
+        }else {
+            System.out.println("Partido ya disputado");
+        }
+    }
+
+    public void mostrarDatos(){
+        System.out.printf("%s:%d VS %d:%s%n ",equipoLocal.getNombre(),gLocal,gVisitante,equipoVisitante.getNombre());
+    }
+
+    public Equipo getEquipoLocal() {
+        return equipoLocal;
+    }
+
+    public void setEquipoLocal(Equipo equipoLocal) {
+        this.equipoLocal = equipoLocal;
+    }
+
+    public Equipo getEquipoVisitante() {
+        return equipoVisitante;
+    }
+
+    public void setEquipoVisitante(Equipo equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public int getgLocal() {
+        return gLocal;
+    }
+
+    public void setgLocal(int gLocal) {
+        this.gLocal = gLocal;
+    }
+
+    public int getgVisitante() {
+        return gVisitante;
+    }
+
+    public void setgVisitante(int gVisitante) {
+        this.gVisitante = gVisitante;
+    }
+
+    public boolean isJugado() {
+        return jugado;
+    }
+
+    public void setJugado(boolean jugado) {
+        this.jugado = jugado;
+    }
+}
